f95c09a072f594e6ae2db2996183397c323a5a83
unknown <ssharma2@svsu.edu>
2018-06-29 04:51:41 -0400

    arrays in jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,59 +1,94 @@
 'use strict';
 
 console.log('app connected');
-
+var appRoot = document.getElementById('app');
 //JSX -Javascript XML Extension
 var app = {
     title: 'Hello World!',
-    subtitle: 'Hello Michigan',
-    options: ['One', 'Two']
+    subtitle: 'hello',
+    options: []
 };
 
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
 
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    renderFormFunction();
 };
 
-var appRoot = document.getElementById('app');
+var removeAll = function removeAll() {
+    app.options = [];
+    renderFormFunction();
+};
 
-// ReactDOM.render(template2,appRoot)
+//Create a render function that creates a new JSX.
 
-var renderCounterApp = function renderCounterApp() {
+var renderFormFunction = function renderFormFunction() {
     var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            'Count:',
-            count
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'no options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    'options:',
+                    option
+                );
+            })
+        ),
+        [React.createElement(
+            'p',
+            { key: '1' },
+            'One'
+        ), React.createElement(
+            'p',
+            { key: '2' },
+            'Two'
+        )],
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
         ),
         React.createElement(
             'button',
-            { onClick: addOne },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'reset'
+            { onClick: removeAll },
+            'Remove all'
         )
     );
     ReactDOM.render(template, appRoot);
 };
-renderCounterApp();
+
+renderFormFunction();
--- src/app.js
+++ src/app.js
@@ -1,44 +1,58 @@
 console.log('app connected')
-
+var appRoot= document.getElementById('app')
 //JSX -Javascript XML Extension
 var app={
     title:'Hello World!',
-    subtitle:'Hello Michigan',
-    options:['One','Two']
-}
-
-let count=0;
-const addOne=()=>{
-    count++;
-    renderCounterApp();
-}
-const minusOne=()=>{
-    count--;
-    renderCounterApp()
-}
-
-const reset=()=>{
-    count=0;
-    renderCounterApp()
+    subtitle:'hello',
+    options:[]
 }
 
 
-var appRoot= document.getElementById('app')
+const onFormSubmit=(e)=>{
+    e.preventDefault()
+    const option=e.target.elements.option.value;
 
- 
-// ReactDOM.render(template2,appRoot)
+    if(option){
+        app.options.push(option);
+        e.target.elements.option.value='';
+    }
+    renderFormFunction()
+}
 
-const renderCounterApp=()=>{
-    var template= (
+const removeAll=()=>{
+    app.options=[];
+    renderFormFunction();
+}
+
+
+//Create a render function that creates a new JSX.
+
+const renderFormFunction=()=>{
+    const template=(
         <div>
-           <h1>Count:{count}</h1>
-           <button onClick={addOne}>+1</button>
-           <button onClick={minusOne}>-1</button>
-           <button onClick={reset}>reset</button>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length>0 ? 'Here are your options':'no options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                {
+                    app.options.map(option=>{
+                        return <li key={option}>options:{option}</li>
+                    })
+                
+                }
+            </ol>
+                {[<p key="1">One</p>, <p key="2">Two</p>]}
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
+            <button onClick={removeAll}>Remove all</button>
+           
         </div>
-        );
-        ReactDOM.render(template,appRoot);
-
+)
+ReactDOM.render(template,appRoot)
 
 }
-renderCounterApp();
+
+renderFormFunction();
